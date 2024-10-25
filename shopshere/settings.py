04762e7f5c6628db74436b61ff62757c133af5aa
Khunt Jay <81973636+khuntjay@users.noreply.github.com>
2024-10-24 23:39:24 -0500

    Reset Password functionality is added
    Create several pages like reset password, confirm password page , reset done and so on.

--- shopshere/settings.py
+++ shopshere/settings.py
@@ -133,3 +133,11 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 MEDIA_ROOT = BASE_DIR /'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 30
+EMAIL_HOST_USER ='softnieo@gmail.com'
+EMAIL_HOST_PASSWORD ='amoutvgsozkafjjg'
--- shopshere/urls.py
+++ shopshere/urls.py
@@ -19,9 +19,15 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from .settings import MEDIA_ROOT
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('accounts/', include('accounts.urls')),
+
+     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
